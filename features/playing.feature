970a68fc7c963c01c6834681444a9c747e7d06d7
globalavocado <globalavocado@gmail.com>
2014-09-14 15:05:38 +0100

    player can end session, test passed

--- features/playing.feature
+++ features/playing.feature
@@ -8,3 +8,8 @@ Feature: Playing
 		When I click on "click here to register your name"
 		And be on the intro page
 		Then I should be able to enter my name
+
+		Scenario: A player can remove their name
+		Given I am registered to play
+		When I click on "click here to remove your name"
+		Then my name should be removed
--- features/step_definitions/player_steps.rb
+++ features/step_definitions/player_steps.rb
@@ -1,3 +1,9 @@
+Given(/^I am registered to play$/) do
+  visit '/intro'
+  fill_in "player", with: "Kevin"
+  click_button "new game"
+end
+
 When(/^I click on "click here to register your name"$/) do
 	click_button('click here to register your name')
 end
@@ -8,4 +14,12 @@ end
 
 Then(/^I should be able to enter my name$/) do
 	 expect(page).to have_button("new game")
+end
+
+When(/^I click on "click here to remove your name"$/) do
+	click_button ('click here to remove your name')
+end
+
+Then(/^my name should be removed$/) do
+	expect(page).to have_content("Bye bye, your name has been removed!")
 end
